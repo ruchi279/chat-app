ecbf889bfad69d1b262096f825370d6bb3c6af06
Ruchi Singh <ruchi122007@gmail.com>
2026-07-09 00:39:48 +0530

    Fix production API URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chatify-44zc.onrender.com/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://chat-app-8zkl.onrender.com/api",
   withCredentials: true,
 });
